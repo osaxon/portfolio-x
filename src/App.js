611faa002f170aa71932553966a06d8409e22983
Oliver Saxon <oliverrsaxon@gmail.com>
2021-07-20 13:50:53 +0100

    Renabled Sanity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-// import About from "./components/About";
-// import SinglePost from "./components/SinglePost";
-// import Post from "./components/Post";
-// import Project from "./components/Project";
 import NavBar from "./components/NavBar";
 
 
@@ -13,10 +9,6 @@ function App() {
       <NavBar />
         <Switch>
           <Route component={Home} path='/' exact/> 
-          {/* <Route component={About} path='/about' /> 
-          <Route component={SinglePost} path='/post/:slug' /> 
-          <Route component={Post} path='/post' /> 
-          <Route component={Project} path='/project' />  */}
         </Switch>
     </BrowserRouter>
     
